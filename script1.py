f253065711c2a4e51b5ff51efb58b99f3704fc7f
Javier Sanchez Fernandez <sanchezfdez.javier@gmail.com>
2020-11-17 21:30:31 +0100

    Throws error when the user inputs a server number on no-create option

--- script1.py
+++ script1.py
@@ -3,8 +3,9 @@
 import os
 import sys
 from subprocess import call
-# import lxml
 import argparse
+# import lxml
+
 
 #Constat definitions
 ORDER_MODES = ["create", "start", "stop", "release"]
@@ -15,11 +16,17 @@ def parse_Arguments():
     parser = argparse.ArgumentParser()
 
     # Positional mandatory argument
-    parser.add_argument("order", help="possible values create/start/stop/release", type=int)
+    parser.add_argument("order", help="possible values create/start/stop/release", type=str)
     # Optional arguments
     parser.add_argument("-sn", "--serversNumber", help="Help placeholder", type=int, default=2)
     # Parse arguments
     return parser.parse_args()
+
+def check_order_input_value(order):
+    if order in ORDER_MODES:
+        return order
+    else:
+        print("Wrong order. The available order modes are: create, start, stop, release")
 
 def check_servers_input_value(servers_number):
     if(MIN_SERVERS <= servers_number <= MAX_SERVERS):
@@ -27,12 +34,19 @@ def check_servers_input_value(servers_number):
     else:
         print("Unsupported servers number, must be in 1 - 5 range")
     
+def optional_without_create():
+    if ((ARGS['order'] != "create") and (ARGS['serversNumber'] == 2)):
+        print("Wrong input. Server number on create option not expected")
+        quit()
 
 
 if __name__ == '__main__':
     ARGS = parse_Arguments().__dict__
-    ORDER = ARGS['order']
+    ORDER = check_order_input_value(ARGS['order'])
     NSERVERS = check_servers_input_value(ARGS['serversNumber'])
+
+    # Minor bug, it is possible to specify server number if the value is 2
+    optional_without_create()
 
     print(ORDER, NSERVERS)
 
