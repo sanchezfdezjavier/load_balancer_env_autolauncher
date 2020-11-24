6e68aff0b39fa096d9211a6350b230f8969ae4a8
Javier Sánchez Fernández <sanchezfdez.javier@gmail.com>
2020-11-24 16:51:15 +0100

    Tests files removedls

--- test2.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-import subprocess
-from subprocess import call
-
-ps = subprocess.Popen(["sudo", "virsh", "list", "--inactive", "--name"], stdout=subprocess.PIPE)
-output = subprocess.check_output(["xargs", "-r", "-n", "1", "virsh","undefine"], stdin=ps.stdout)
-ps.wait()
